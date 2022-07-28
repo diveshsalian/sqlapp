b75c46d7872a95e3feb8cce4349b24c969d51039
Divesh Salian <diveshsal.2006@gmail.com>
2022-07-28 13:32:40 +0530

    exception enabled

--- sqlapp/Program.cs
+++ sqlapp/Program.cs
@@ -8,11 +8,11 @@ builder.Services.AddTransient<IProductService, ProductService>();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
-
+app.UseDeveloperExceptionPage();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+  
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }

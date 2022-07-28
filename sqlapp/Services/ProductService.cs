a9c17dc81480aee5f06aac67be9733721e0c049d
Divesh Salian <diveshsal.2006@gmail.com>
2022-07-28 13:25:44 +0530

    added connection string

--- sqlapp/Services/ProductService.cs
+++ sqlapp/Services/ProductService.cs
@@ -5,35 +5,28 @@ namespace sqlapp.Services
 {
 
     // This service will interact with our Product data in the SQL database
-    public class ProductService
+    public class ProductService : IProductService
     {
-        private static string db_source = "firstsql.database.windows.net";
-        private static string db_user = "sysadmin";
-        private static string db_password = "login@123";
-        private static string db_database = "firstsql";
-
+        private readonly IConfiguration _configuration;
+        public ProductService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         private SqlConnection GetConnection()
         {
 
-            var _builder = new SqlConnectionStringBuilder
-            {
-                DataSource = db_source,
-                UserID = db_user,
-                Password = db_password,
-                InitialCatalog = db_database
-            };
-            return new SqlConnection(_builder.ConnectionString);
+            return new SqlConnection(_configuration.GetConnectionString("SQLConnection"));
         }
         public List<Product> GetProducts()
         {
-            List<Product> _product_lst = new();
+            List<Product> _product_lst = new List<Product>();
             string _statement = "SELECT ProductID,ProductName,Quantity from Products";
             SqlConnection _connection = GetConnection();
-            
+
             _connection.Open();
-            
+
             SqlCommand _sqlcommand = new SqlCommand(_statement, _connection);
-            
+
             using (SqlDataReader _reader = _sqlcommand.ExecuteReader())
             {
                 while (_reader.Read())
